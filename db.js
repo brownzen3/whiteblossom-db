1f36c63a05c1e20442b3d4793a6fd1d3931d3388
Zen <87526909+brownzen3@users.noreply.github.com>
2023-08-30 19:27:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,9 +3,9 @@
 
 
 // WEBSITE CONTROL
-const websiteStatus = 0
+const websiteStatus = 1
 // 1: Normal  /  0: Disabled
-const websiteMessage = "Under Maintainance"
+const websiteMessage = "Under Scheduled Maintainance"
 // Message displayed under "Website Access Temporarily Suspended" heading
 // (only displayed when websiteStatus = 0)
 
