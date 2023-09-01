e7e9828ed6fac948740c0089563436ad30dbce54
Zen <87526909+brownzen3@users.noreply.github.com>
2023-09-01 09:38:18 +0530

    Create control.js

---
+++ control.js
@@ -0,0 +1,7 @@
+// WEBSITE CONTROL
+const websiteStatus = 1
+// 1: Normal  /  0: Disabled
+
+const websiteMessage = "Under Scheduled Maintainance"
+// Message displayed under "Website Access Temporarily Suspended" heading
+// (only displayed when websiteStatus = 0)
