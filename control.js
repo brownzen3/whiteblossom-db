0bfb6ad7acbc5e93d5d346476485bbbb2b2cfd30
Zen <87526909+brownzen3@users.noreply.github.com>
2023-09-01 10:13:32 +0530

    Update control.js

--- control.js
+++ control.js
@@ -1,7 +1,7 @@
 // WEBSITE CONTROL
-const websiteStatus = 1
+const websiteStatus = 0
 // 1: Normal  /  0: Disabled
 
-const websiteMessage = "Testing Cache Configuration #2"
+const websiteMessage = "Testing Cache Configuration #3"
 // Message displayed under "Website Access Temporarily Suspended" heading
 // (only displayed when websiteStatus = 0)
