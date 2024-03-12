1eeb432b28bee3899f85b3ad4205a8f48a4614a3
Zen <87526909+brownzen3@users.noreply.github.com>
2024-03-12 18:32:09 +0000

    Update control.js

--- control.js
+++ control.js
@@ -1,11 +1,16 @@
+// Use this document to alter the visibility of the website.
+// NOTICE: Changes may not appear immediately due to webpage caching.
+//         To remove the website immediatley, log into Cloudflare.
+
+
+// Website visibility
+// 1: Normal  /  0: Hide
+
 const websiteStatus = 1
 
-// 1: Normal  /  0: Disabled
 
-
-
-
-const websiteMessage = "Site Under Scheduled Maintenance<br>Estimated Completion: 1 Jan, 12:00"
-
+// Website roadblock message
 // Message displayed under "Website Access Temporarily Suspended" heading
 // (only displayed when websiteStatus = 0)
+
+const websiteMessage = "Site Under Scheduled Maintenance<br>Estimated Completion: 1 Jan, 12:00"
